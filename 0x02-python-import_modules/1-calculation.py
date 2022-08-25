1aca9e6027a16aa343146203d4d1d9eb55f3a08d
Franco <vandenberg.fw@gmail.com>
2022-08-25 18:52:00 +0000

    1. My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-
-if len(sys.argv) == 1:
-    print("{} arguments.".format(len(sys.argv) - 1))
-elif len(sys.argv) == 2:
-    print("{} argument:".format(len(sys.argv) - 1))
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-
-for a in range(1, len(sys.argv)):
-    print("{}: {}".format(a, sys.argv[a]))
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
